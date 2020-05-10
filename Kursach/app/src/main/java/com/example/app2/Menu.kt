cefa2592af824e90848bfcab91597decfcda0110
Sanya <sanyacorotkih@yandex.ru>
2020-05-10 15:53:05 +0300

    Lera skazala

--- Kursach/app/src/main/java/com/example/app2/Menu.kt
+++ Kursach/app/src/main/java/com/example/app2/Menu.kt
@@ -1,14 +1,10 @@
 package com.example.app2
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.View
-import android.widget.TextView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -20,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.app2.ui.cart.TableUserOrder
 import com.example.app2.ui.menu.TableMenu
-import com.example.app2.ui.order.DBHelperAllOrders
 import com.example.app2.ui.order.TableOrders
 
 class Menu : AppCompatActivity() {
